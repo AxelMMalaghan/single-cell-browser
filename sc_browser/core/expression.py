c9fd34136f01dc978308508ca4954ee969b26c85
axel <axelm@malaghan.org.n>
2025-12-05 10:41:14 +1300

    feat: added 3d for cluster graphs

--- sc_browser/core/expression.py
+++ sc_browser/core/expression.py
@@ -1,32 +1,9 @@
-import numpy as np
-import pandas as pd
-import scipy
+from __future__ import annotations
 
 from typing import List
-from anndata import AnnData
 
-
-def get_dense_matrix(self, adata: AnnData, layer: str = None, max_cells: int = 10000) -> np.ndarray:
-    """
-    Safely returns a dense matrix from .X or a specified layer. Raises is matrix is too large to convert.
-    :param self:
-    :param adata:
-    :param layer:
-    :param max_cells:
-    :return:
-    """
-    X = adata.layers[layer] if layer else adata.X
-
-    # only warn or block if it's sparse
-    if hasattr(X, "toarray"):
-        nume1 = X.shape[0] * X.shape[1]
-        if nume1 > max_cells * X.shape[1]:
-            raise MemoryError("Attempted to densify {X.shape[0]}×{X.shape[1]} matrix "
-                              f"({nume1:,} elements). This may crash your system.")
-        return X.toarray()
-
-    return X
-
+import pandas as pd
+import scipy
 
 
 def extract_expression_matrix(self, adata, genes: List[str]) -> pd.DataFrame:
@@ -48,5 +25,3 @@ def extract_expression_matrix(self, adata, genes: List[str]) -> pd.DataFrame:
 
     df.index = adata.obs_names
     return df
-
-
